2b1c2fec57c736a5977cd45aca5810b7b5cf168d
Kevin werk PC <kevin@hcomputers.nl>
2015-05-04 11:25:51 +0200

    Added top functionality and started work on the score update functionality

---
+++ StageFly/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace StageFly
+{
+    public class Player
+    {
+        public int PlayerId { get; set; }
+        public string Name { get; set; }
+        public int Rank { get; set; }
+    }
+}
---
+++ StageFly/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace StageFly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[StageFly]Enter Command:");
+            var command = Console.ReadLine().ToLower();
+
+            switch (command)
+            {
+                case "gettop":
+                    var players = GetTop();
+                    Console.WriteLine(DisplayTop(players));
+                    break;
+                default:
+                    Console.WriteLine("Command not recognised");
+                    break;
+            }
+        }
+
+        private static string DisplayTop(IEnumerable<Player> players)
+        {
+            var text = "TOP 25:\n";
+
+            foreach (var player in players)
+            {
+                text += String.Format("{0}. {1} (playerId: {2})\n", player.Rank, player.Name, player.PlayerId);
+            }
+
+            return text;
+        }
+
+        private static IEnumerable<Player> GetTop()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_apiEndPoint);
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public static int CalculatePoints(int score)
+        {
+            return (score * 13) + 295;
+        }
+
+        private const string _apiEndPoint = "http://api.dancetourgos.nl";
+    }
+}
